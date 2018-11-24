e43a92937938cb9205dc4b4e023c0c6bfb5c44e2
04an <olegdzhum@gmail.com>
2018-11-24 15:58:30 +0100

    Dodane metoda sprawdzenia wieku

--- src/main/java/HotelService/Model/Guest.java
+++ src/main/java/HotelService/Model/Guest.java
@@ -1,11 +1,18 @@
 package HotelService.Model;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Guest {
 
     private String name;
-    private  String lastName;
+    private String lastName;
     private LocalDate dateOfBirth;
+
+
+    public boolean isAdult(){
+        Period period = Period.between(dateOfBirth, LocalDate.now());
+        return period.getYears()>=18;
+    }
 
 }
